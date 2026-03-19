f32ecd7cd01b88a93e72c94f1fa1ffb8d5af74a2
Sayan Saha <sahasayan100@gmail.com>
2026-03-19 22:54:44 +0530

    Changes to retrieval tool

--- api/routers/rag.py
+++ api/routers/rag.py
@@ -57,7 +57,7 @@ async def create_embeddings(file: UploadFile = File(...)):
     return CreateEmbeddingsResponseList(embedding_ids=_ids, count=len(_ids))
 
 @router.post("/search")
-async def search(query: str, top_k: int = 5, score_threshold: float = 0.5, search_type: Literal["similarity", "similarity_score_threshold", "mmr"] = "similarity_score_threshold", lambda_mult: float = 0.5):
+async def search(query: str):
     '''Endpoint to search for relevant documents in the vector database based on a query.'''
     try:
         results = await retrieval_tool(query=query, top_k=top_k, score_threshold=score_threshold, search_type=search_type, lambda_mult=lambda_mult)
--- mcp_server/tools/retrieval_tool.py
+++ mcp_server/tools/retrieval_tool.py
@@ -4,4 +4,5 @@ from modules.rag.vector_store import VectorDBService
 async def retrieval_tool(query: str, top_k: int = 5, score_threshold: float = 0.5, search_type: str = "similarity_score_threshold", lambda_mult: float = 0.5) -> str:
     '''A tool function that takes a query as input and returns relevant information retrieved from the vector database.'''
     vector_db = VectorDBService()
-    return await vector_db.search(query, top_k=top_k, score_threshold=score_threshold, search_type=search_type, lambda_mult=lambda_mult)
+    docs = await vector_db.search(query)
+    return "\n\n---\n\n".join(doc.page_content for doc in docs)
